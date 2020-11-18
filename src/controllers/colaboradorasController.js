5f3ecece759698660e273a831daaf96f61689111
TalitaFraga <talitad.fraga@gmail.com>
2020-11-18 19:28:56 -0300

    add authorization

--- src/controllers/colaboradorasController.js
+++ src/controllers/colaboradorasController.js
@@ -1,32 +1,29 @@
 const colaboradoras = require('../models/colaboradoras.js');
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const SECRET = process.env.SECRET 
+const SECRET = process.env.SECRET
+
+
 
 const postColaboradora = (req, res) => {
     const senhaComHash = bcrypt.hashSync(req.body.senha, 10)
     req.body.senha = senhaComHash
     let colaboradora = new colaboradoras(req.body)
-
-
     colaboradora.save(function(err){
     if(err) { 
         res.status(500).send({ message: err.message })
     }
         res.status(201).send(colaboradora.toJSON())
     })
-    
-    };
+};
 
-    const login = (req, res) => {
-
-        colaboradoras.findOne({email:req.body.email}, function(error, colaboradora){
+const login = (req, res) => {
+    colaboradoras.findOne({email:req.body.email}, function(error, colaboradora){
         if (!colaboradora) {
             return res.status(404).send(`Não existe colaboradora com o email ${req.body.email}`)
         }
         const senhaValida = bcrypt.compareSync(req.body.senha, colaboradora.senha) 
         
-
         if (!senhaValida) {
             return res.status(403).send('que senha é essa!')
         }
@@ -34,10 +31,7 @@ const postColaboradora = (req, res) => {
         const token = jwt.sign({ email: req.body.email }, SECRET)
     
         return res.status(200).send(token)
-    
     })
-        
-        
-    }
+}
 
     module.exports = { postColaboradora, login };
