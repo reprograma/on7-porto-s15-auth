7fc99b553f249e3fe5b6fe5c08f1b09ae375998d
NadyLuna <nadyedja@gmail.com>
2020-11-24 16:56:52 -0300

    Ajustes

--- src/controllers/tarefasController.js
+++ src/controllers/tarefasController.js
@@ -1,108 +1,101 @@
-const tarefas = require('../models/tarefas');
-const SECRET = process.env.SECRET
-const jwt = require('jsonwebtoken')
+const tarefas = require("../models/tarefas");
+const SECRET = process.env.SECRET;
+const jwt = require("jsonwebtoken");
 
 const getAll = (req, res) => {
-  const authHeader = req.get ('authorization');
+  const authHeader = req.get("authorization");
   if (!authHeader) {
-    return res.status(401).send('oxe, cadê os negoço do header?')
+    return res.status(401).send("oxe, cadê os negoço do header?");
   }
-    const token = authHeader.split('') [1];  
-  jwt.verify(token, SECRET, function(erro) {
+  const token = authHeader.split("")[1]; jwt.verify(token, SECRET, function (erro) {
     if (erro) {
-      return res.status(403).send('Nãm')
+      return res.status(403).send("Nãm");
     }
-    tarefas.find(function(err, tarefas){
-      if(err) { 
-        res.status(500).send({ message: err.message })
+    tarefas.find(function (err, tarefas) {
+      if (err) {
+        return res.status(424).send({
+          message: err.message,
+        });
+      } else {
+        return res.status(200).send(tarefas);
       }
-      res.status(200).send(tarefas);
-    })
+    });
   });
 };
-  
-  
 
 const getById = (req, res) => {
   const id = req.params.id;
-  const authHeader = req.get ('authorization');
+  const authHeader = req.get("authorization");
   if (!authHeader) {
-    return res.status(401).send('oxe, cadê os negoço do header?')
-  }
-    const token = authHeader.split('') [1];  
-  jwt.verify(token, SECRET, function(erro) {
+    return res.status(401).send("oxe, cadê os negoço do header?");
+  };
+  const token = authHeader.split("")[1]; jwt.verify(token, SECRET, function (erro) {
     if (erro) {
-      return res.status(403).send('Nãm')
-    }
-  tarefas.find({ id }, function(err, tarefas){
-    if(err) { 
-      res.status(500).send({ message: err.message })
-    }
-    res.status(200).send(tarefas);
-    })
-  })
+      return res.status(403).send("Nãm");
+    };
+    tarefas.find({id,},
+      function (err, tarefas) {
+        if (err) {
+          return res.status(424).send({
+            message: err.message,
+          });
+        } else {
+          return res.status(200).send(tarefas);
+        }
+      }
+    );
+  });
 };
 
 const postTarefa = (req, res) => {
-  const authHeader = req.get ('authorization');
+  const authHeader = req.get("authorization");
   if (!authHeader) {
-    return res.status(401).send('oxe, cadê os troço do header?')
+    return res.status(401).send("oxe, cadê os troço do header?");
   }
-    const token = authHeader.split('') [1];  
-  jwt.verify(token, SECRET, function(erro) {
+  const token = authHeader.split("")[1]; jwt.verify(token, SECRET, function (erro) {
     if (erro) {
-      return res.status(403).send('Nãm')
+      return res.status(403).send("Nãm");
     }
-    
-  let tarefa = new tarefas(req.body)
-  tarefa.save(function(err){
-    if(err) { 
-      res.status(500).send({ message: err.message })
-    }
-    res.status(201).send(tarefa.toJSON())
-    })
-  })
+
+    let tarefa = new tarefas(req.body);
+    tarefa.save(function (err) {
+      if (err) {
+        res.status(500).send({
+          message: err.message,
+        });
+      };
+      res.status(201).send(tarefa.toJSON());
+    });
+  });
 };
 
 const deleteTarefa = (req, res) => {
   const id = req.params.id;
-  const authHeader = req.get('authorization');
-    if (!authHeader) {
-    return res.status(401).send('E o Header Criatura?!');
+  const authHeader = req.get("authorization");
+  if (!authHeader) {
+    return res.status(401).send("E o Header Criatura?!");
   };
-  const token = authHeader.split(' ')[1];
-  jwt.verify(token, SECRET, err => {
+    const token = authHeader.split(" ")[1]; jwt.verify(token, SECRET, (err) => {
     if (err) {
-      return res.status(403).send('Token inválido');
-    }; 
-    tarefas.find({ id }, function(err, tarefa){
-    if(tarefa.length > 0){
-      tarefas.deleteMany({ id }, function(err){
-        if(err) { 
-          res.status(500).send({ 
-            message: err.message, 
-            status: "FAIL" 
-          })
-        }
-        res.status(200).send({ 
-          message: 'Tarefa removida com sucesso', 
-          status: "SUCCESS" 
-        })
-      })
-    }else{
-      res.status(200).send({ 
-        message: 'Não há tafera para ser removida', 
-        status: "EMPTY" 
-      
-      })
-    }
-    
-  })
-})
+      return res.status(403).send("Token inválido");
+    };
+    tarefas.find({id,}, function (err, tarefa) {
+        if (tarefa.length > 0) { tarefas.deleteMany({id,},
+            function (err) {
+              if (err) {
+                return res.status(500).send({ message: err.message, status: "FAIL",});
+              };
+              return res.status(200).send({message: "Tarefa removida com sucesso", status: "SUCCESS",});
+            });
+          } else {
+          return res.status(200).send({ message: "Não há tafera para ser removida", status: "EMPTY",
+          });
+      };
+    });
+  });
 };
 
 const deleteTarefaConcluida = (req, res) => {
-  //Deleta quando concluido = true
   const authHeader = req.get('authorization');
 
   if (!authHeader) {
@@ -115,49 +108,47 @@ const deleteTarefaConcluida = (req, res) => {
     if (err) {
       return res.status(403).send('Token inválido');
     };
-  try {
-    tarefas.deleteMany({ concluido: true }, function (err) {
-        if (!err) {
-            res.status(200).send({ message: 'Tarefas concluidas removidas com sucesso', status: "SUCCESS" })
-        }
-    })
-  } catch (err) {
-    console.log(err)
-    return res.status(424).send({ message: err.message })
-      }
-    })
-  }
 
-
+    tarefas.find({ concluido: true }, (err, tarefa) => {
+      if (tarefa.length > 0) {tarefas.deleteMany({ concluido: true }, err => {
+          if (err) {
+            return res.status(424).send({ message: err.message });
+          };
+          return res.status(200).send('Tarefa(s) deletada(s) com sucesso');
+        });
+      };
+    });
+  });
+};
 
 const putTarefa = (req, res) => {
   const id = req.params.id;
-  const authHeader = req.get('authorization');
+  const authHeader = req.get("authorization");
 
   if (!authHeader) {
-    return res.status(401).send('Header não encontrado');
-  };
+    return res.status(401).send("Header não encontrado");
+  }
 
-  const token = authHeader.split(' ')[1];
-
-  jwt.verify(token, SECRET, err => {
+  const token = authHeader.split(" ")[1]; jwt.verify(token, SECRET, (err) => {
     if (err) {
-      return res.status(403).send('Token inválido');
-    };
-  tarefas.find({ id }, function(err, tarefa){
-    if(tarefa.length> 0){     
-      tarefas.updateMany({ id }, { $set : req.body }, function (err) {
-        if (err) {
-          res.status(500).send({ message: err.message })
+      return res.status(403).send("Token inválido");
+    }
+    tarefas.find({id,},function (err, tarefa) {
+        if (tarefa.length > 0) {tarefas.updateMany({id,}, {$set: req.body,},
+            function (err) {
+              if (err) {
+                res.status(500).send({message: err.message,});
+              };
+              res.status(200).send({message: "Registro alterado com sucesso",});
+            }
+          );
+        } else {
+          res.status(200).send({ message: "Não há registros para serem atualizados com esse id",});
         }
-        res.status(200).send({ message: "Registro alterado com sucesso"})
-      })
-    }else {
-      res.status(200).send({ message: "Não há registros para serem atualizados com esse id"})
-        }
-      })
-    })
-}
+      }
+    );
+  });
+};
 
 module.exports = {
   getAll,
@@ -165,5 +156,5 @@ module.exports = {
   postTarefa,
   deleteTarefa,
   deleteTarefaConcluida,
-  putTarefa
-}
+  putTarefa,
+};
